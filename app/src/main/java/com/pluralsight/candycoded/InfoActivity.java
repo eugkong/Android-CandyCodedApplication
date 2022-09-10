8e2a3f5a5121de2ee5875c4cdeec7aecc4baecf7
Eugene Kong <eugkong@gmail.com>
2022-09-10 20:35:19 +0800

    Start the Phone Activity

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -42,5 +42,12 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+    public void createPhoneIntent(View view) {
+        final Uri uri = Uri.parse("tel:0123456789");
+//        final Intent phoneIntent = new Intent(Intent.ACTION_DIAL, uri);
+        final Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
+        phoneIntent.setData(uri);
+        startActivity(phoneIntent);
+    }
 
 }
